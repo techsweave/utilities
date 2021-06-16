f0ea947c3c402525351c9ae620398b02f4052fcc
Urbani Simone <urb.simone@gmail.com>
2021-06-16 16:57:24 +0200

    Add user email

--- src/auth/authenticatedUser.ts
+++ src/auth/authenticatedUser.ts
@@ -1,9 +1,11 @@
 import AWS from 'aws-sdk';
+import { Email } from 'aws-sdk/clients/codecommit';
 
 export class AuthenticatedUser {
 
     private _provider: AWS.CognitoIdentityServiceProvider;
     private _userId: string;
+    private _email: Email;
 
     /**
      * @async
@@ -17,12 +19,14 @@ export class AuthenticatedUser {
      */
     static async fromToken(token: string): Promise<AuthenticatedUser> {
         const res = new AuthenticatedUser();
-        res._userId = (
-            await res._provider
-                .getUser({
-                    AccessToken: token
-                }).promise())
-            .Username;
+        const user = (await res._provider
+            .getUser({
+                AccessToken: token
+            }).promise());
+
+        res._userId = user.Username;
+        res._email = user.UserAttributes['email'];
+
         return Promise.resolve(res);
     }
 
@@ -41,6 +45,17 @@ export class AuthenticatedUser {
      */
     public async getUserId(): Promise<string> {
         return Promise.resolve(this._userId);
+    }
+
+    /**
+     * @async
+     *
+     * @summary Get the user email
+     * 
+     * @return {Promise<string>} Cognito User Email
+     */
+    public async getEmail(): Promise<string> {
+        return Promise.resolve(this._email);
     }
 
     /**
